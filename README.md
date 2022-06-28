f8e79d303d85a05250eae41c3911d138af552a79
Nikola Bozhinov <n.bozhinov@gmail.com>
2022-06-29 01:50:32 +0300

    Finished the script for checking out the TensorFlow source code and building the C++ bindings. Updated the TRT and cuda versions requirements for 2.9.0 and latest master. Cleaned up repo structure.

--- README.md
+++ README.md
@@ -5,10 +5,10 @@ Setting up ARM based machines (embedded devices) is not yet supported, but is pl
 
 
 ## Usage
-***python3 configure.py --tf-version <1.15.3 | 2.3.0 | 2.4.0 | 2.9.0> --local-repo-dir \<path to the directory with the TensorRT's local repo .deb files\> --output-dir \<path to the directory to save the script\>***  
+***python3 configure.py --tf-version <1.15.3 | 2.3.0 | 2.4.0 | 2.9.0 | latest> --local-repo-dir \<path to the directory with the TensorRT's local repo .deb files\> --output-dir \<path to the directory to save the script\>***  
 
-This creates two scripts - **install_bazel.sh** and **install_prerequisites**.sh, which install all packages needed for development with the specific version of TensorFlow using TensorRT for GPU acceleration.  
-In addition **build_tensorflow_cpp.sh** can be used to create and install the C++ bindings of TensorFlow, which have to be build from source. They don't have official binary distribution, but are officially supported.
+This creates three scripts in the **./output** directory - , **install_prerequisites.sh**, **install_bazel.sh** and **build_tensorflow_cpp.sh**. The first one installs all packages needed for development with the specific version of TensorFlow using TensorRT for GPU acceleration.  
+The other two can be used to create and install the C++ bindings of TensorFlow, which have to be build from source. They don't have official binary distribution, but are officially supported.
 
 ## Docker
-[The NVIDIA Container Toolkit](https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/install-guide.html)
+Using GPU hardware acceleration through a docker container requires additional runtime fom Nvidia. Instructions on how to set it up are available in [The NVIDIA Container Toolkit](https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/install-guide.html) webpage.
--- configure.py
+++ configure.py
@@ -33,7 +33,8 @@ tf_versions_map = {
         "TRT_VER_FULL" : "6.0.1.5-1",
         "NVINFER_VER" : "6.0.1-1",
         "TRT_VER_MAJOR" : "6",
-        "BAZEL_VERSION" : "0.26.1"
+        "BAZEL_VERSION" : "0.26.1",
+        "PIP_REQUIREMENTS_FILE" : "requirements_1.15.3.txt"
     },
     "2.0.0" : {
         "TENSORRT_REPO_FILE" : "nv-tensorrt-repo-ubuntu1804-cuda10.0-trt6.0.1.5-ga-20190913_1-1_amd64.deb",
@@ -47,7 +48,8 @@ tf_versions_map = {
         "TRT_VER_FULL" : "6.0.1.5-1",
         "NVINFER_VER" : "6.0.1-1",
         "TRT_VER_MAJOR" : "6",
-        "BAZEL_VERSION" : "0.26.1"
+        "BAZEL_VERSION" : "0.26.1",
+        "PIP_REQUIREMENTS_FILE" : "requirements_1.15.3.txt"
     },
     "2.3.0" : {
         "TENSORRT_REPO_FILE" : "nv-tensorrt-repo-ubuntu1804-cuda11.0-trt7.2.2.3-ga-20201211_1-1_amd64.deb",
@@ -61,7 +63,8 @@ tf_versions_map = {
         "TRT_VER_FULL" : "7.2.2.3-1",
         "NVINFER_VER" : "7.2.2-1",
         "TRT_VER_MAJOR" : "7",
-        "BAZEL_VERSION" : "3.1.0"
+        "BAZEL_VERSION" : "3.1.0",
+        "PIP_REQUIREMENTS_FILE" : "requirements_2.4.0.txt"
     },
     "2.4.0" : {
         "TENSORRT_REPO_FILE" : "nv-tensorrt-repo-ubuntu1804-cuda11.0-trt7.2.2.3-ga-20201211_1-1_amd64.deb",
@@ -75,9 +78,25 @@ tf_versions_map = {
         "TRT_VER_FULL" : "7.2.2.3-1",
         "NVINFER_VER" : "7.2.2-1",
         "TRT_VER_MAJOR" : "7",
-        "BAZEL_VERSION" : "3.1.0"
+        "BAZEL_VERSION" : "3.1.0",
+        "PIP_REQUIREMENTS_FILE" : "requirements_2.4.0.txt"
     },
     "2.9.0" : {
+        "TENSORRT_REPO_FILE" : "nv-tensorrt-repo-ubuntu2004-cuda11.4-trt8.2.5.1-ga-20220505_1-1_amd64.deb",
+        "TENSORRT_REPO_NAME" : "nv-tensorrt-repo-ubuntu2004-cuda11.4-trt8.2.5.1-ga-20220505",
+        "TENSORRT_REPO_DOWNLOAD_URL" : "https://developer.nvidia.com/compute/machine-learning/tensorrt/secure/8.2.5.1/local_repos/nv-tensorrt-repo-ubuntu2004-cuda11.4-trt8.2.5.1-ga-20220505_1-1_amd64.deb",
+        "CUDA_VER_NUM" : "11.4",
+        "CUDA_VER_MAJOR" : "11",
+        "CUDA_VER_MINOR" : "4",
+        "CUDA_VER_TEXT" : "cuda11.4",
+        "CUDA_VER_DASH" : "11-4",
+        "TRT_VER_FULL" : "8.2.5.1-1",
+        "NVINFER_VER" : "8.2.5-1",
+        "TRT_VER_MAJOR" : "8",
+        "BAZEL_VERSION" : "5.0.0",
+        "PIP_REQUIREMENTS_FILE" : "requirements_2.9.0.txt"
+    },
+    "latest" : {
         "TENSORRT_REPO_FILE" : "nv-tensorrt-repo-ubuntu2004-cuda11.6-trt8.4.1.5-ga-20220604_1-1_amd64.deb",
         "TENSORRT_REPO_NAME" : "nv-tensorrt-repo-ubuntu2004-cuda11.6-trt8.4.1.5-ga-20220604",
         "TENSORRT_REPO_DOWNLOAD_URL" : "https://developer.nvidia.com/compute/machine-learning/tensorrt/secure/8.4.1/local_repos/nv-tensorrt-repo-ubuntu2004-cuda11.6-trt8.4.1.5-ga-20220604_1-1_amd64.deb",
@@ -89,13 +108,16 @@ tf_versions_map = {
         "TRT_VER_FULL" : "8.4.1.5-1",
         "NVINFER_VER" : "8.4.1-1",
         "TRT_VER_MAJOR" : "8",
-        "BAZEL_VERSION" : "5.0.0"
+        "BAZEL_VERSION" : "5.1.1",
+        "PIP_REQUIREMENTS_FILE" : "requirements_2.9.0.txt"
     }
 }
 
+templates_dir = Path(__file__).parent.absolute() / "template_scripts"
 bash_script_files = [
-    Path(__file__).parent.absolute() / "install_prerequisites.sh.in",
-    Path(__file__).parent.absolute() / "install_bazel.sh.in"
+    templates_dir / "install_prerequisites.sh.in",
+    templates_dir / "install_bazel.sh.in",
+    templates_dir / "build_tensorflow_cpp.sh.in"
 ]
 
 output_directory = Path("./output")
@@ -116,14 +138,19 @@ if __name__ == "__main__":
         bash_script_file_out = output_directory / str(bash_script_file_in.name)[0 : -3]
         with open(bash_script_file_in, 'r') as script_in, open(bash_script_file_out, 'w') as script_out:
             script_template = script_in.read()
+
+            if args.tf_version == "latest":
+                script_template = script_template.replace("@TENSORFLOW_VERSION@", "master")
+            else:
+                script_template = script_template.replace("@TENSORFLOW_VERSION@", "v" + args.tf_version)
+            script_template = script_template.replace("@ARCH@", arch)
+            script_template = script_template.replace("@UBUNTU_VERSION_NAME@", ubuntu_version_name)
+            script_template = script_template.replace("@UBUNTU_VERSION_NUMERIC@", ubuntu_version_numeric)
+
             for key, value in tf_versions_map[args.tf_version].items():
                 if key == "TENSORRT_REPO_FILE":
                     script_template = script_template.replace("@" + key + "@", str(Path(args.local_repo_dir) / value))
                 else:
                     script_template = script_template.replace("@" + key + "@", value)
-            script_template = script_template.replace("@ARCH@", arch)
-            script_template = script_template.replace("@UBUNTU_VERSION_NAME@", ubuntu_version_name)
-            script_template = script_template.replace("@UBUNTU_VERSION_NUMERIC@", ubuntu_version_numeric)
             script_out.write(script_template)
         os.chmod(bash_script_file_out, stat.S_IRWXU | stat.S_IRWXG)
-
